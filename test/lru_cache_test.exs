688af6da0d3ca2606fab67a16cbb27938c6e8a1d
vrtineu <vininro@gmail.com>
2024-03-31 12:33:32 -0300

    Refactor LRUCacheTest code

--- test/lru_cache_test.exs
+++ test/lru_cache_test.exs
@@ -1,17 +1,15 @@
 defmodule LRUCacheTest do
   use ExUnit.Case
-  doctest LRUCache
 
   describe "LRUCache" do
     test "case 1" do
-      # ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
-      # [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+      ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+      [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
 
       LRUCache.init_(2)
 
       assert LRUCache.put(1, 1) == :ok
       assert LRUCache.put(2, 2) == :ok
-      # [1, {2,2}] should be [{2,2}, {1,1}]
       assert LRUCache.get(1) == 1
       assert LRUCache.put(3, 3) == :ok
       assert LRUCache.get(2) == -1
